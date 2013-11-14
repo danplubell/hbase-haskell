ade0b770fedf0c0585819e6c8eda610206c2dff7
Dan Plubell <dplubell@cerner.com>
2013-11-14 16:30:03 -0600

    add get column descriptors, clean up connection definition

--- src/Database/Hbase/Client.hs
+++ src/Database/Hbase/Client.hs
@@ -1,27 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Database.Hbase.Client
 (
-    HBaseConnectionSource(..),
-    HBaseColumnDescriptor(..),
-    HBaseConnection(..),
-    QualifiedColumnName(..),
-    Put(..),
-    TableName,
-    RowKey,
-    Value,
-    defaultHBaseConnectionSource,
-    defaultColumnDescriptor,
-    openConnection,
-    closeConnection,
-    createTable,
-    getTableNames,
-    disableTable,
-    deleteTable,
-    enableTable,
-    compact,
-    majorCompact,
-    putRow,
-    getRow 
+      HBaseConnectionSource(..)
+    , HBaseColumnDescriptor(..)
+    , HBaseConnection(..)
+    , QualifiedColumnName(..)
+    , Put(..)
+    , TableName
+    , RowKey
+    , Value
+    , defaultHBaseConnectionSource
+    , defaultColumnDescriptor
+    , openConnection
+    , closeConnection
+    , createTable
+    , getTableNames
+    , disableTable
+    , deleteTable
+    , enableTable
+    , compact
+    , majorCompact
+    , getColumnDescriptors
+    , putRow
+    , getRow 
 ) where
 
 import qualified    Data.ByteString.Lazy        as BL
@@ -43,10 +44,11 @@ import qualified    Data.HashMap.Strict as HashMap
 import              Data.Int
 
 ------------Data Structures-------------------
-type TableName = String  
-type TableRegionName = String  
-type RowKey = BL.ByteString
-type Value = BL.ByteString
+type TableName          = String  
+type TableRegionName    = String  
+type RowKey             = BL.ByteString
+type Value              = BL.ByteString
+type ColumnName         = String
 
 data HBaseConnectionSource = HBaseConnectionSource
     {
@@ -64,36 +66,37 @@ defaultHBaseConnectionSource = HBaseConnectionSource
 
 data HBaseColumnDescriptor  = HBaseColumnDescriptor 
     {
-          columnName                :: Maybe String
-        , columnMaxVersions         :: Maybe Int32
-        , columnCompression         :: Maybe String
-        , columnInMemory            :: Maybe Bool
-        , columnBloomFilterType     :: Maybe String
-        , columnFilterVectorSize    :: Maybe Int32
-        , columnFilterNbHashes      :: Maybe Int32
-        , columnCacheEnabled        :: Maybe Bool
-        , columnTimeToLive          :: Maybe Int32
+          columnName                    :: Maybe String
+        , columnMaxVersions             :: Maybe Int32
+        , columnCompression             :: Maybe String
+        , columnInMemory                :: Maybe Bool
+        , columnBloomFilterType         :: Maybe String
+        , columnBloomFilterVectorSize   :: Maybe Int32
+        , columnBloomFilterNbHashes     :: Maybe Int32
+        , columnBlockCacheEnabled       :: Maybe Bool
+        , columnTimeToLive              :: Maybe Int32
    }deriving (Show)
 
 data HBaseConnection = HBaseConnection
     {
-          handle              :: Handle
-        , connectionSource    :: HBaseConnectionSource  
-        , connectionProtocol  :: BinaryProtocol Handle
+          handle                :: Handle
+        , connectionSource      :: HBaseConnectionSource  
+        , connectionProtocol    :: BinaryProtocol Handle
+        , connectionIpOp        :: (BinaryProtocol Handle, BinaryProtocol Handle)
     }
 
 defaultColumnDescriptor :: HBaseColumnDescriptor     
 defaultColumnDescriptor=HBaseColumnDescriptor 
     {
-          columnName                = Nothing
-        , columnMaxVersions         = Nothing
-        , columnCompression         = Nothing
-        , columnInMemory            = Nothing
-        , columnBloomFilterType     = Nothing
-        , columnFilterVectorSize    = Nothing
-        , columnFilterNbHashes      = Nothing
-        , columnCacheEnabled        = Nothing
-        , columnTimeToLive          = Nothing
+          columnName                    = Nothing
+        , columnMaxVersions             = Nothing
+        , columnCompression             = Nothing
+        , columnInMemory                = Nothing
+        , columnBloomFilterType         = Nothing
+        , columnBloomFilterVectorSize   = Nothing
+        , columnBloomFilterNbHashes     = Nothing
+        , columnBlockCacheEnabled       = Nothing
+        , columnTimeToLive              = Nothing
     }       
 
 data QualifiedColumnName = QualifiedColumnName 
@@ -104,27 +107,27 @@ data QualifiedColumnName = QualifiedColumnName
 
 data Put = Put
     {
-          qualifiedColumn::QualifiedColumnName
-        , value::Value
+          qualifiedColumn      ::QualifiedColumnName
+        , value                ::Value
     }deriving (Show)
     
 data RowResultValue = RowResultValue
     {
-          rowResultValue :: Maybe BL.ByteString
-        , rowResultTimeStamp:: Maybe Int64
+          rowResultValue        :: Maybe BL.ByteString
+        , rowResultTimeStamp    :: Maybe Int64
     }deriving (Show) 
     
 data RowResultColumn = RowResultColumn
     {
-          rowResultColumn :: Maybe String
-        , rowResultColumnValue :: Maybe RowResultValue
+          rowResultColumn       :: Maybe String
+        , rowResultColumnValue  :: Maybe RowResultValue
     }deriving (Show)
 
 data RowResult = RowResult 
     {
-         rowResultKey :: Maybe RowKey
-       , rowResultColumns:: Maybe (HashMap.HashMap String RowResultValue)
-       , rowResultSortedColumns::Maybe (Vector.Vector RowResultColumn)
+         rowResultKey           :: Maybe RowKey
+       , rowResultColumns       :: Maybe (HashMap.HashMap ColumnName RowResultValue)
+       , rowResultSortedColumns :: Maybe (Vector.Vector RowResultColumn)
     }deriving (Show)
     
 ------------Functions-------------------------
@@ -132,7 +135,11 @@ data RowResult = RowResult
 openConnection :: HBaseConnectionSource -> IO HBaseConnection
 openConnection c = do
     h <- hOpen (hostName c, port c)
-    return $ HBaseConnection{ connectionSource = c, handle = h, connectionProtocol = BinaryProtocol h}
+    return $ HBaseConnection{ connectionSource = c
+                            , handle = h
+                            , connectionProtocol = BinaryProtocol h
+                            , connectionIpOp = (BinaryProtocol h, BinaryProtocol h)
+    }
 
 closeConnection :: HBaseConnection -> IO()
 closeConnection c = tClose $ handle c
@@ -145,12 +152,12 @@ createTable t l c =
 
 getTableNames :: HBaseConnection -> IO [TableName]
 getTableNames conn = do
-        vector <- HClient.getTableNames (connectionProtocol conn, connectionProtocol conn)
+        vector <- HClient.getTableNames (connectionIpOp conn)
         return $ map (BC.unpack . lazyToStrict) $ Vector.toList vector
 
 putRow::TableName -> RowKey->[Put]->HBaseConnection -> IO()
 putRow t r p c = do
-        HClient.mutateRow (connectionProtocol c, connectionProtocol c) (strToLazy t) r (putsToMutations p) HashMap.empty
+        HClient.mutateRow (connectionIpOp c) (strToLazy t) r (putsToMutations p) HashMap.empty
 
 getRow::TableName->RowKey->HBaseConnection->IO (Vector.Vector RowResult)
 getRow t r conn = do
@@ -158,23 +165,25 @@ getRow t r conn = do
     return $ Vector.map tRowResultToRowResult results
 
 disableTable::TableName -> HBaseConnection -> IO()
-disableTable t c= HClient.disableTable (connectionProtocol c, connectionProtocol c) (strToLazy t)
+disableTable t c= HClient.disableTable (connectionIpOp c) (strToLazy t)
 
 deleteTable::TableName -> HBaseConnection -> IO()
-deleteTable t c = HClient.deleteTable (connectionProtocol c, connectionProtocol c) (strToLazy t)
+deleteTable t c = HClient.deleteTable (connectionIpOp c) (strToLazy t)
 
 enableTable :: TableName -> HBaseConnection ->IO()
-enableTable t c = HClient.enableTable (connectionProtocol c, connectionProtocol c) (strToLazy t) 
+enableTable t c = HClient.enableTable (connectionIpOp c) (strToLazy t) 
 
 compact :: TableRegionName -> HBaseConnection -> IO()
-compact tr c= HClient.compact (connectionProtocol c, connectionProtocol c) (strToLazy tr) 
+compact tr c= HClient.compact (connectionIpOp c) (strToLazy tr) 
 
 majorCompact :: TableRegionName -> HBaseConnection -> IO()
-majorCompact tr c = HClient.majorCompact (connectionProtocol c, connectionProtocol c) (strToLazy tr)
+majorCompact tr c = HClient.majorCompact (connectionIpOp c) (strToLazy tr)
 
- 
-
-
+getColumnDescriptors ::TableName-> HBaseConnection -> IO (HashMap.HashMap ColumnName HBaseColumnDescriptor)
+getColumnDescriptors t c = do
+    results <- HClient.getColumnDescriptors (connectionIpOp c) (strToLazy t)
+    return $    HashMap.fromList $ map  (\(n,c) -> (lazyToString n, hColDescFromfColDesc c)) (HashMap.toList results)
+    
 -----------Utility Functions-----------------
 --convert a string to list of Word8
 strToWord8s :: String -> [Word8]
@@ -186,6 +195,8 @@ strToLazy = BL.pack.strToWord8s
 lazyToStrict :: BL.ByteString -> BS.ByteString
 lazyToStrict lazy = BS.concat $ BL.toChunks lazy
 
+lazyToString:: BL.ByteString -> String
+lazyToString lazy = BC.unpack . BS.concat $ BL.toChunks lazy
 
 fColDescFromHColDesc::HBaseColumnDescriptor -> ColumnDescriptor
 fColDescFromHColDesc d  = ColumnDescriptor 
@@ -195,11 +206,26 @@ fColDescFromHColDesc d  = ColumnDescriptor
     , f_ColumnDescriptor_compression            = fmap TL.pack $ columnCompression d
     , f_ColumnDescriptor_inMemory               = columnInMemory d
     , f_ColumnDescriptor_bloomFilterType        = fmap TL.pack $ columnBloomFilterType d
-    , f_ColumnDescriptor_bloomFilterVectorSize  = columnFilterVectorSize d
-    , f_ColumnDescriptor_bloomFilterNbHashes    = columnFilterNbHashes d
-    , f_ColumnDescriptor_blockCacheEnabled      = columnCacheEnabled d
+    , f_ColumnDescriptor_bloomFilterVectorSize  = columnBloomFilterVectorSize d
+    , f_ColumnDescriptor_bloomFilterNbHashes    = columnBloomFilterNbHashes d
+    , f_ColumnDescriptor_blockCacheEnabled      = columnBlockCacheEnabled d
     , f_ColumnDescriptor_timeToLive             = columnTimeToLive d
   }
+  
+hColDescFromfColDesc::ColumnDescriptor -> HBaseColumnDescriptor
+hColDescFromfColDesc d = HBaseColumnDescriptor
+    {
+          columnName                    = fmap lazyToString $ f_ColumnDescriptor_name d
+        , columnMaxVersions             = f_ColumnDescriptor_maxVersions d
+        , columnCompression             = fmap TL.unpack $ f_ColumnDescriptor_compression d
+        , columnInMemory                = f_ColumnDescriptor_inMemory d
+        , columnBloomFilterType         = fmap TL.unpack $ f_ColumnDescriptor_bloomFilterType d
+        , columnBloomFilterVectorSize   = f_ColumnDescriptor_bloomFilterVectorSize d
+        , columnBloomFilterNbHashes     = f_ColumnDescriptor_bloomFilterNbHashes d
+        , columnBlockCacheEnabled       = f_ColumnDescriptor_blockCacheEnabled d
+        , columnTimeToLive              = f_ColumnDescriptor_timeToLive d
+        
+    }  
 convertQualifiedColumn::QualifiedColumnName-> BL.ByteString
 convertQualifiedColumn c = strToLazy $ qualifiedColumnFamily c ++ ":" ++ qualifiedColumnName c
 
